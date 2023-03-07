80f6ee73bd0f82d4ea5ad770d2c3dbf3e0563f9c
Fathima Thasneem <fathimathasneem3621@gmail.com>
2023-03-07 19:53:18 +0530

    message

--- myapp/src/Search.js
+++ myapp/src/Search.js
@@ -1,27 +1,37 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import './search.css';
+import "./search.css";
 
 export default function Search() {
-    const navigate = useNavigate()
+  const navigate = useNavigate();
 
-    return(
-        <div>
-
-<div className="search-box">
+  return (
+    <div>
+      <div className="search-box">
         <form action="">
-            <input href="contactforblood" type="submit" onClick={(e) => {
-                e.preventDefault()
-                navigate('/search-by-blood')
-            }} name="" value="Search by Blood Group"/><br></br>
-            <br></br>
-            <input type="submit" onClick={(e) => {
-                e.preventDefault()
-                navigate('/search-donor')
-            }} name="" value="Search Donor by Place"/>
-            
+          <input
+            href="contactforblood"
+            type="submit"
+            onClick={(e) => {
+              e.preventDefault();
+              navigate("/search-by-blood");
+            }}
+            name=""
+            value="Search by Blood Group"
+          />
+          <br></br>
+          <br></br>
+          <input
+            type="submit"
+            onClick={(e) => {
+              e.preventDefault();
+              navigate("/search-donor");
+            }}
+            name=""
+            value="Search Donor by Place"
+          />
         </form>
+      </div>
     </div>
-        </div>
-    )
+  );
 }
